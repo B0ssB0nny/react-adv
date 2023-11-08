75759ace73bd50dd1f213985369c9fb89fb6ef73
John <johnari151298@gmail.com>
2023-11-08 11:59:04 +0100

    lazyload Basico Listo

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,6 @@
 import {lazy, LazyExoticComponent} from 'react'
+import { NoLazy } from '../01-lazyload/pages/NoLazy';
+
 
 type JSXComponent = () => JSX.Element;
 
@@ -9,27 +11,20 @@ interface Route {
     name: string;
 }
 
-const lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1'))
-const lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage2'))
-const lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3'))
+const Lazylayout = lazy(() => import(/* webpackChunkName: "LazyLayout" */ '../01-lazyload/layout/LazyLayout'))
+
 
 export const routes: Route[] = [
     {
-        to: '/lazy1',
-        path: 'lazy1',
-        Component: lazy1,
+        to: '/lazyload/',
+        path: '/lazyload/*',
+        Component: Lazylayout,
         name: 'Lazy-1'
     },
     {
-        to: '/lazy2',
-        path: 'lazy2',
-        Component: lazy2,
-        name: 'Lazy-2'
-    },
-    {
-        to: '/lazy3',
-        path: 'lazy3',
-        Component: lazy3,
-        name: 'Lazy-3'
+        to: '/no-lazy',
+        path: 'no-lazy',
+        Component: NoLazy,
+        name: 'No Lazy'
     },
 ]
